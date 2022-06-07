40dfec4cd6fabb4a8f7a1abce4585b112b322f8b
Bishal Mishra <bishaller@gmail.com>
2022-06-07 18:53:35 +0545

    add: static files

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index_page
+from .views import index_page, single_page
 
 urlpatterns = [
     path('', index_page, name='index'), 
+    path('student/<int:id>', single_page, name='single'), 
 ]
